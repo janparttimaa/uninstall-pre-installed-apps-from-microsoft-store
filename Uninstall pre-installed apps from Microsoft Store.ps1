24c158e7c80383c5b78373b986ae3596280aa0ad
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2024-03-14 10:17:49 +0200

    Create Uninstall pre-installed apps from Microsoft Store.ps1

---
+++ Uninstall pre-installed apps from Microsoft Store.ps1
@@ -0,0 +1,91 @@
+$appname = @(
+        # OneNote for Windows 10
+        "Microsoft.Office.OneNote"
+        # Mail and Calendar
+        "microsoft.windowscommunicationsapps"
+        # Cortana
+        "Microsoft.549981C3F5F10"
+        # App Installer
+        "Microsoft.DesktopAppInstaller"
+        # Get Help
+        "Microsoft.GetHelp"
+        # 3D Viewer
+        "Microsoft.Microsoft3DViewer"
+        # Microsoft 365 (Office)
+        "Microsoft.MicrosoftOfficeHub"
+        # Microsoft Solitaire Collection
+        "Microsoft.MicrosoftSolitaireCollection"
+        # Mixed Reality Portal
+        "Microsoft.MixedReality.Portal"
+        # People
+        "Microsoft.People"
+        # Skype
+        "Microsoft.SkypeApp"
+        # Store Purchase App
+        "Microsoft.StorePurchaseApp"
+        # Microsoft.VCLibs.140.00
+        "Microsoft.VCLibs.140.00"
+        # VP9 Video Extensions
+        "Microsoft.VP9VideoExtensions"
+        # Microsoft Pay
+        "Microsoft.Wallet"
+        # Web Media Extensions
+        "Microsoft.WebMediaExtensions"
+        # Webp Image Extensions
+        "Microsoft.WebpImageExtension"
+        # Feedback Hub
+        "Microsoft.WindowsFeedbackHub"
+        # Maps
+        "Microsoft.WindowsMaps"
+        # Xbox Game UI
+        "Microsoft.Xbox.TCUI"
+        # Xbox
+        "Microsoft.XboxApp"
+		    "Microsoft.GamingApp"
+        "Microsoft.XboxSpeechToTextOverlay"
+        # Xbox Game Bar
+        "Microsoft.XboxGamingOverlay"
+        "Microsoft.XboxGameOverlay"
+        # Xbox Identity Provider
+        "Microsoft.XboxIdentityProvider"
+        # Phone Link
+        "Microsoft.YourPhone"
+        # Groove Music / Windows Media Player
+        "Microsoft.ZuneMusic"
+        # Movies & TV
+        "Microsoft.ZuneVideo"
+        # Microsoft Clipchamp
+        "Clipchamp.Clipchamp"
+        # Microsoft To-Do
+        "Microsoft.Todos"
+        # Dev Home
+        "Microsoft.Windows.DevHome"
+        # Microsoft News
+        "Microsoft.BingNews"
+        # Microsoft Teams Personal
+        "MicrosoftTeams"
+		    # Microsoft Whiteboard
+		    "Microsoft.Whiteboard"
+		    # Power Automate
+		    "Microsoft.PowerAutomateDesktop"
+		    # Quick Assist
+		    "MicrosoftCorporationII.QuickAssist"
+		    # Paint 3D
+		    "Microsoft.MSPaint"
+    )
+
+    # Remove apps from current user
+    ForEach($app in $appname){
+    Get-AppxPackage -Name $app | Remove-AppxPackage -ErrorAction SilentlyContinue
+    }
+
+    # Remove apps from all users - may need to reboot after running above and run this again
+    ForEach($app in $appname){
+    Get-AppxPackage -Allusers -Name $app | Remove-AppxPackage -Allusers -ErrorAction SilentlyContinue
+    }
+
+    # Remove apps from provisioned apps list so they don't reinstall on new users
+    ForEach($app in $appname){
+    Get-AppxProvisionedPackage -Online | where {$_.PackageName -like $app} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+    }
+timeout /t 20 /nobreak 
